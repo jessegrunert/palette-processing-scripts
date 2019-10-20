52c2f8581612041a02e373b30e88f41c01889c4a
Jesse Grunert <jesse@grunert.ca>
2019-10-20 13:36:23 -0700

    started work on generating the omega

--- parse_total_filament.py
+++ parse_total_filament.py
@@ -1,6 +1,7 @@
 import struct
 import sys
 import re
+import math
 
 
 def chomp(x):
@@ -14,6 +15,16 @@ def hexify_float(f):
 
     return "D{:0>8}".format(_number)
 
+def hexify_short(num):
+    # hexify_short: Converts a short integer into the specific notation used by Mosaic
+    if num < 0:
+        num += 65536
+    return "D" + '{0:04x}'.format(num)
+
+def hexify_long(num):
+    # hexify_long: Converts a 32-bit integer into the specific notation used by Mosaic
+    return "D" + '{0:08x}'.format(num)
+
 input_file = sys.argv[1]
 output_file = input_file + "_annotated"
 filament_total = 0.00
@@ -22,11 +33,16 @@ line_buffer_length = 10
 max_filament_segment_length = 0.00
 did_g92 = False
 
-pings = [350.00, 700.00, 2100.00, 5000.00, 10000.00, 25000.00, 50000.00]
-ping_position = 0
+pings = [350.00]
+ping = 0
+ping_increment = 350
+
+layers = []
+layer = 0
 
 evalue_pattern = re.compile('G1\sX([0-9\.]*)\sY([0-9\.]*)\sE([0-9\.]*)')
 retract_pattern = re.compile('G1\sE([0-9\.]*)')
+layer_pattern = re.compile('^;\sLayer:\s([0-9]*)')
 
 file_in = open(input_file,"r")
 file_out = open(output_file, "w")
@@ -60,42 +76,80 @@ for line in file_in_lines:
 
         file_out.write(line)
 
+        # count layers
+    layer_match = layer_pattern.match(line)
+    if layer_match:
+        layers.insert(layer, filament_total)
+        layer += 1
+
     # insert pings
-    if filament_total >= pings[ping_position]:
+    if filament_total >= pings[ping]:
         file_out.write("G4 P0\n")
         file_out.write("O31 " + hexify_float(filament_total))
         file_out.write("\n")
-        ping_position += 1
+        pings.append(math.ceil(filament_total + ping_increment))
+        ping_increment = math.ceil(ping_increment * 1.03)
+        ping += 1
 
-    
+# remove the last ping as it will never get used.
+del pings[-1]
+ping -= 1
+
 file_in.close()
 file_out.close()
 
-#O21 D0014; don't change
-#O22 D93dc44b0d6fa4540; ID of printer
-#O23 D0001; don't change
-#O24 D0000; don't change
-#O25 D1000000Black_PLA D1FF1515FireBrick_PLA D0 D0; 
-#O26 D000F; Number of splices, short hex
-#O27 D0007; Number of pings, short hex
-#O28 D0001; no idea, never changed it
-#O29 D0000; no idea, never changed it
-#O30 D0 D456297ec; floating point hex of splice point
-#O30 D1 D45a12f31; floating point hex of splice point
-#O30 D0 D45d83f31; floating point hex of splice point
-#O30 D1 D460b3335; floating point hex of splice point
-#O30 D0 D462d9934; floating point hex of splice point
-#O30 D1 D46520302; floating point hex of splice point
-#O30 D0 D467698e2; floating point hex of splice point
-#O30 D1 D468c903d; floating point hex of splice point
-#O30 D0 D469bbcdb; floating point hex of splice point
-#O30 D1 D46a83295; floating point hex of splice point
-#O30 D0 D46b232df; floating point hex of splice point
-#O30 D1 D46bab5f4; floating point hex of splice point
-#O30 D0 D46c2e190; floating point hex of splice point
-#O30 D1 D46cbc8a9; floating point hex of splice point
-#O30 D0 D46dac000; floating point hex of splice point add extra lenght above what print uses for bowden tube.
-#O32 D11 D0000 D0000 D0000; never changed it, dunno what it does.
-#O1 D40_Layer_Stripe_Vase D00006D60; Description, two lines up, but Hex long, rounded up.
-#M0
-#T0
+print("Total filament length is: " + str(filament_total) + "mm")
+print("Total number of layers: " + str(len(layers)))
+
+how_many_layers = input("How many layers would you like?")
+print("Using " + str(how_many_layers) + " layers")
+
+layers_per_color = math.ceil(int(len(layers)) / int(how_many_layers))
+print("Using " + str(layers_per_color) + " layers per color.")
+
+print("Layer splice table")
+for i, val in enumerate(layers):
+    print("Layer: ", i, " | ", val, "mm")
+
+print("Ping table")
+for i, val in enumerate(pings):
+    print("Ping: ", i, " | ", val, "mm")
+
+#build out the omega.
+tool_order = [0, 1, 2, 3]
+omega = []
+tool_index = 0
+bowden_offset = 500
+
+omega.append("O21 D0014")
+omega.append("O22 D93dc44b0d6fa4540")
+omega.append("O23 D0001")
+omega.append("O24 D0000")
+omega.append("O25 D1000000Black_PLA D1FF1515FireBrick_PLA D10BF728GreenYellow_PLA D1FFFF00Yellow_PLA ")
+omega.append("O26 " + hexify_short(int(how_many_layers)))
+omega.append("O27 " + hexify_short(ping))
+omega.append("O28 D0001")
+omega.append("O29 D0000")
+
+
+for i, val in enumerate(layers):
+    if i > 0:
+        if (i / layers_per_color).is_integer():
+            # we're on a layer that needs a splice
+            omega.append("O30 D" + str(tool_order[tool_index]) + " " + hexify_float(val))
+            tool_index += 1
+            if tool_index >= len(tool_order):
+                tool_index = 0
+
+
+# add in the final splice which is the end of the print + bowden tube offset
+final_splice = math.ceil(filament_total + bowden_offset)
+omega.append("O30 D" + str(tool_order[tool_index]) + " " + hexify_float(float(final_splice)))
+
+omega.append("O32 D11 D0000 D0000 D0000")
+omega.append("O1 DRainbow_Pet_Dish " + hexify_long(final_splice))
+omega.append("M0")
+omega.append("T0")
+
+for o in omega:
+    print(o)
